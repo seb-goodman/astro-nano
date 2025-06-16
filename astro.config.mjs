1a416ee1b32ebd63b6531a6f4a1ac7829f99fb32
seb-goodman <sebgoodman01@gmail.com>
2025-06-16 15:00:22 +0100

    Remove vercel satic config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercelStatic from "@astrojs/vercel/static";
 
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
   output: "static",
-  adapter: vercelStatic({
-    analytics: true,
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
 });
